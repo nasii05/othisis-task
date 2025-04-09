8a3bbf8a79e682a17556d11f4635de19a5cbab82
nasii05 <iamnasih@gmail.com>
2025-04-09 17:46:05 +0530

    drag n drop implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'othisis-task';
+
+  
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    CommonModule,
+    CdkDropList,
+    CdkDrag
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+   items = [
+    {name: 'one', logo: ''},
+    {name: 'two', logo: ''},
+    {name: 'three', logo: ''},
+    {name: 'four', logo: ''},
+    {name: 'five', logo: ''},
+    {name: 'six', logo: ''},
+   ]
+
+   templates = [
+    'Subjective', 'Objective', 'Assessment & Plan', 'Findings',
+    'Diagnoses', 'Treatment', 'Recovery', 'Objective',
+  ];
+
+  noteSections = [
+    { title: 'Subjective', points: ['Toothache for few days'] },
+    { title: 'History of Presenting Complaints', points: ['Toothache: present for few days'] },
+    { title: 'Extra Oral Examination', points: ['Not performed'] },
+    { title: 'Intra Oral Examination', points: ['Tenderness around molar', 'Swollen gum'] },
+    { title: 'Radiographic Findings', points: ['X-ray planned to confirm extent of infection'] },
+    { title: 'Diagnoses', points: ['Suspected tooth abscess'] },
+    { title: 'Intra Oral Examination', points: ['Tenderness around molar', 'Swollen gum'] },
+    { title: 'Radiographic Findings', points: ['X-ray planned to confirm extent of infection'] },
+    { title: 'Diagnoses', points: ['Suspected tooth abscess'] },
+    { title: 'Intra Oral Examination', points: ['Tenderness around molar', 'Swollen gum'] },
+    { title: 'Radiographic Findings', points: ['X-ray planned to confirm extent of infection'] },
+    { title: 'Diagnoses', points: ['Suspected tooth abscess'] },
+    { title: 'Intra Oral Examination', points: ['Tenderness around molar', 'Swollen gum'] },
+    { title: 'Radiographic Findings', points: ['X-ray planned to confirm extent of infection'] },
+    { title: 'Diagnoses', points: ['Suspected tooth abscess'] },
+  ];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.noteSections, event.previousIndex, event.currentIndex);
+  }
+}
